bd0050b70717a8c5f05857e5aab1df591abe7103
Nicklas <Nicklas@stardust.local>
2017-01-27 20:54:37 +0100

    Chapter 2.6

--- Receipt1/ViewController.swift
+++ Receipt1/ViewController.swift
@@ -8,28 +8,54 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController
+{
 
     let array = ["item1", "item2", "item3"]
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.rowHeight = 70;
+        tableView.backgroundView = UIImageView(image: UIImage(named: "Mountains"))
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationController?.navigationBar.alpha = 0.5
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         return array.count;
     }
     
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "customcell")! as UITableViewCell;
-        cell.textLabel?.text = array[indexPath.item];
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customcell")! as UITableViewCell
+        
+        if (indexPath.item % 2 == 0)
+        {
+            cell.backgroundColor = UIColor.clear
+        }
+        else
+        {
+            cell.backgroundColor = UIColor.white.withAlphaComponent(0.2);
+            cell.textLabel?.backgroundColor = UIColor.white.withAlphaComponent(0.0)
+        }
+        
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.text = array[indexPath.item]
         
         return cell;
     }
